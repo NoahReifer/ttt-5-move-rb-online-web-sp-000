d3876bd0cfa94a91b3ef47755cd750262ad26b0a
Noah Reifer <sreifer4@gmail.com>
2019-06-27 19:38:03 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -1,9 +1,9 @@
-def display_board(board)
-  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+def display_board(array)
+  puts " #{array[0]} | #{array[1]} | #{array[2]} "
   puts "-----------"
-  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts " #{array[3]} | #{array[4]} | #{array[5]} "
   puts "-----------"
-  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+  puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
 
 def input_to_index(userput)
@@ -13,3 +13,9 @@ end
 def move(array, index, value = "X")
   array[index] = value
 end
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
